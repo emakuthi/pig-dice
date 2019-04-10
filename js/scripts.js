847dd7e6c4977915a585036d19a0fe7a9ee95a4a
Elvis Nzioki Makuthi <EMakuthi@safaricom.co.ke>
2019-04-10 18:49:46 +0300

    initialize logic for both business & UI

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+// **********Business logic************
+function rollDice(){
+  return Math.floor((Math.random() * 6) + 1);
+}
+
+// ************User interface logic************
+
+$(document).ready(){
+ 
+}
