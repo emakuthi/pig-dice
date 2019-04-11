0a076aeb6e16ea76458fdfa2d219a09f1e423878
Elvis Nzioki Makuthi <EMakuthi@safaricom.co.ke>
2019-04-11 19:06:15 +0300

    add for loop for pushing scores to array

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,65 @@ var totalRoll1 = 0;
 var totalRoll2 = 0;
 
 var randomizer = function (){
+  
   return Math.floor(Math.random() * 6) + 1;
 }
 
+var randomizer2 = function (){
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function player (name, score){
+  this.playerName = name;
+  this.scores = new Array();
+
+}
+
+var player1 = new Player()
+var player2 = new player()
+
+scores = [];
+for (var i=1, t=6; i<t; i++) {
+  arr.push(randomizer());
+}
+
+var totalScore = 0;
+
+
 
 $(document).ready(
-
-
   function(){$("#p1").click(function(){
+   
+  var roll1 = randomizer();
+  totalRoll1 += roll1;
+  $("#alert-2").text(" ")
   
-  var roll = randomizer();
-  totalRoll1 += roll;
-  
-  if (roll!==1){
-    $("#player1 h3").text(roll);
-    $("#player1 h4").text(totalRoll1);
+  if (roll1!==1){
+    $("#player1 h3").text(roll1);
+    $("#p2").prop("disabled",true);
+    $("#p2").css({"background": "grey", "box-shadow":"0 0 grey" });
+    $("#p22").css({"background": "grey", "box-shadow":"0 0 grey", "transition": "0s" });
+    $("#alert-2").text("") ;
   }
   else{
      totalRoll1 ===0;
      $("#alert-1").text("oops! you have lost all your points") ;
      $("#player1 h3").text("0");
-     $("#player1 h4").text("0"); 
+     $("#player1 h4").text("your total points are:"+" "+ totalRoll1);
+     $("#p1").prop("disabled",true);
+     $("#p1").css({"background": "grey", "box-shadow":"0 0 grey" });
+     $("#p12").css({"background": "grey", "box-shadow":"0 0 grey", "transition": "0s" });
+     $("#p2").prop("disabled",false);
+     $("#p2").css({"background": "#802915", "box-shadow":"0 3px #802915" });
+     $("#p22").css({"background": "#802915", "box-shadow":"0 3px #802915", "transition": ".3s" });
   }
   
 })
 
 $("#p12").click(function(){
-  totalRoll1 += totalRoll1;
+  // totalRoll1 += totalRoll1;
   
-  $("#player1 h3").text("HOLD");
+  $("#player1 h3").text("0");
   $("#player1 h4").text("your score for this round is"+" "+ totalRoll1);
   
 })
@@ -38,10 +68,11 @@ $("#p12").click(function(){
 
 
 $("#p2").click(function(){
-  var roll = randomizer();
-  totalRoll2+= roll;
-  if (roll!==1){
-    $("#player2 h3").text(roll);
+  var roll2 = randomizer2();
+  totalRoll2+= roll2;
+  $("#alert-1").text(" ")
+  if (roll2!==1){
+    $("#player2 h3").text(roll2);
     $("#p1").prop("disabled",true);
     $("#p1").css({"background": "grey", "box-shadow":"0 0 grey" });
     $("#p12").css({"background": "grey", "box-shadow":"0 0 grey", "transition": "0s" });
@@ -63,10 +94,9 @@ $("#p2").click(function(){
 })
 
 $("#p22").click(function(){
-  totalRoll2 += totalRoll2;
-  
-  $("#player2 h3").text("HOLD");
-  $("#player2 h4").text(totalRoll2);
+  // totalRoll2 += totalRoll2;
+  $("#player2 h3").text("0");
+  $("#player2 h4").text("your score for this round is"+" "+ totalRoll2);
   
 })
 
@@ -78,6 +108,8 @@ $("#restart").click(function(){
   $("#player1 h4").text("0");
   $("#player2 h3").text("0");
   $("#player2 h4").text("0");
+  totalRoll1 == 0;
+  totalRoll2 == 0;
   
 })
 if(totalRoll1 === 100 || totalRoll2===100){
