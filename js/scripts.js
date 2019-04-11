e22a5bc74a293ded857911cee418408a4cf94ba3
Elvis Nzioki Makuthi <EMakuthi@safaricom.co.ke>
2019-04-11 14:33:23 +0300

    styles the player classes

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,24 @@ var randomizer = function (){
 }
 
 
-$(document).ready(function(){$("#p1").click(function(){
+$(document).ready(
+
+
+  function(){$("#p1").click(function(){
   
   var roll = randomizer();
   totalRoll1 += roll;
   
-  $("#player1 h3").text(roll);
-  $("#player1 h4").text(totalRoll1);
-  
-  $("#p1").prop("disabled", true);
-  
+  if (roll!==1){
+    $("#player1 h3").text(roll);
+    $("#player1 h4").text(totalRoll1);
+  }
+  else{
+     totalRoll1 ===0;
+     alert("oops! you have lost all your points") 
+     $("#player1 h3").text("0");
+     $("#player1 h4").text("0"); 
+  }
   
 })
 
@@ -23,19 +31,25 @@ $("#p12").click(function(){
   totalRoll1 += totalRoll1;
   
   $("#player1 h3").text("HOLD");
-  $("#player1 h4").text(totalRoll1);
+  $("#player1 h4").text("your score for this round is"+" "+ totalRoll1);
   
 })
 
 
 
 $("#p2").click(function(){
-  
   var roll = randomizer();
   totalRoll2+= roll;
-  
-  $("#player2 h3").text(roll);
-  $("#player2 h4").text(totalRoll2);
+  if (roll!==1){
+    $("#player2 h3").text(roll);
+    $("#player2 h4").text(totalRoll2);
+  }
+  else{
+     totalRoll2 ===0;
+     alert("oops! you have lost all your points") 
+     $("#player2 h3").text("0");
+     $("#player2 h4").text("0"); 
+  }
   
 })
 
@@ -51,9 +65,14 @@ $("#restart").click(function(){
   totalRoll1 = 0;
   totalRoll2 = 0;
   
-  $("#player1 h3").text("");
-  $("#player1 h4").text("");
-  $("#player2 h3").text("");
-  $("#player2 h4").text("");
+  $("#player1 h3").text("0");
+  $("#player1 h4").text("0");
+  $("#player2 h3").text("0");
+  $("#player2 h4").text("0");
   
-})})
+})
+if(totalRoll1 === 100 || totalRoll2===100){
+  alert("game over!!!");
+}
+
+})
