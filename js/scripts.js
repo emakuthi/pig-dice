baec73b9a749e657f9cffc1e42cf56a99b2cfc94
Elvis Nzioki Makuthi <EMakuthi@safaricom.co.ke>
2019-04-11 07:10:30 +0300

    creates containers

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,61 @@
 
-// **********Business logic************
-function rollDice(){
-  return Math.floor((Math.random() * 6) + 1);
+var totalRoll1 = 0;
+var totalRoll2 = 0;
+
+var randomizer = function (){
+  return Math.floor(Math.random() * 6) + 1;
 }
 
-// ************User interface logic************
 
-$(document).ready(){
- 
-}
+$("#p1").click(function(){
+  
+  var roll = randomizer();
+  totalRoll1 += roll;
+  
+  $("#player1 h3").text(roll);
+  $("#player1 h4").text(totalRoll1);
+  
+  $("#p1").prop("disabled", true);
+  
+  
+})
+
+$("#p12").click(function(){
+  totalRoll1 += totalRoll1;
+  
+  $("#player1 h3").text("HOLD");
+  $("#player1 h4").text(totalRoll1);
+  
+})
+
+
+
+$("#p2").click(function(){
+  
+  var roll = randomizer();
+  totalRoll2+= roll;
+  
+  $("#player2 h3").text(roll);
+  $("#player2 h4").text(totalRoll2);
+  
+})
+
+$("#p22").click(function(){
+  totalRoll2 += totalRoll2;
+  
+  $("#player2 h3").text("HOLD");
+  $("#player2 h4").text(totalRoll2);
+  
+})
+
+$("#restart").click(function(){
+  totalRoll1 = 0;
+  totalRoll2 = 0;
+  
+  $("#player1 h3").text("");
+  $("#player1 h4").text("");
+  $("#player2 h3").text("");
+  $("#player2 h4").text("");
+  
+})
+
