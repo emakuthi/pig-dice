3787e9c6c347bc0742da0dd2fc4fc79f59158f0c
Elvis Nzioki Makuthi <EMakuthi@safaricom.co.ke>
2019-04-13 11:57:42 +0300

    add winni logic

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,16 @@ Player.prototype.hold = function(){
   this.totalScore += this.currentRound.reduce(function getSum(total, amount){return total + amount;});
 }
 
+var diceImage = function (){
+
+  if(player1.die ===1 || player2.die === 1){$("#dice-pic").atrr("src", "images/inverted-dice-1.png");}
+  else if(player1.die ===2 || player2.die === 2){$("#dice-pic").atrr("src", "images/inverted-dice-2.png");}
+  else if(player1.die ===3 || player2.die === 3){$("#dice-pic").atrr("src", "images/inverted-dice-3.png");}
+  else if(player1.die ===4 || player2.die === 4){$("#dice-pic").atrr("src", "images/inverted-dice-4.png");}
+  else if(player1.die ===5 || player2.die === 5){ $("#dice-pic").atrr("src", "images/inverted-dice-5.png");}
+  else if(player1.die ===6 || player2.die === 6){ $("#dice-pic").atrr("src", "images/inverted-dice-6.png");}
+  else{$("#dice-pic").atrr("src", "images/pig.png");}
+}
 
 // ***************user interface logic******************
 
@@ -36,15 +46,13 @@ Player.prototype.hold = function(){
 $(document).ready(
   function(){$("#p1").click(function(){
    
- 
+  // diceImage();
   $("#alert-2").text(" ")
   player1.roll();
-  player1.currentRound === [];
+  player1.currentRound === [0];
   
   if (player1.die!==1){
-    $("#player1 h3").text([player1.currentRound]);
-    $("#p2").hide();
-    $("#p22").hide();
+    $("#player1 h3").text([player1.currentRound]);$("#p2").hide();$("#p22").hide();
     $("#alert-2").text("") ;
   }
   else{
@@ -52,10 +60,7 @@ $(document).ready(
      $("#alert-1").text("oops! you have lost all your points") ;
      $("#player1 h3").text(" 0 ");
     //  $("#player1 h4").text("your total points are:"+" "+ player1.totalScore);
-    $("#p1").hide();
-    $("#p12").hide();
-    $("#p2").show();
-    $("#p22").show();
+    $("#p1").hide();$("#p12").hide();$("#p2").show();$("#p22").show();
   }
   
 })
@@ -63,13 +68,16 @@ $(document).ready(
 $("#p12").click(function(){
   // totalRoll1 += totalRoll1;
   player1.hold();
+  if(player1.totalScore>=100){
+    $("#alert-winner1").text(" YOU'RE THE WINNER!!!");
+    $("#player1 h3").text("0");
+    $("#player1 h4").text("TOTAL SCORE:"+" "+ player1.totalScore);
+    $("#p1").hide();$("#p12").hide();$("#p2").hide();$("#p22").hide();
+  }else{
   player1.currentRound = [];
   $("#player1 h3").text("0");
-  $("#player1 h4").text("your score for this round is"+" "+ player1.totalScore);
-  $("#p1").hide();
-  $("#p12").hide();
-  $("#p2").show();
-  $("#p22").show();
+  $("#player1 h4").text("TOTAL SCORE:"+" "+ player1.totalScore);
+  $("#p1").hide();$("#p12").hide();$("#p2").show();$("#p22").show();}
 })
 
 
@@ -79,47 +87,36 @@ $("#p2").click(function(){
   player2.roll();
   $("#player2 h3").text(" ");
   if (player2.die!==1){
-    $("#player2 h3").text(player2.currentRound);
-    $("#p1").hide();
-    $("#p12").hide();
+    $("#player2 h3").text(player2.currentRound);$("#p1").hide();$("#p12").hide();
   }
   else{
+
     player2.currentRound === [];
      $("#alert-2").text("oops! you have lost all your points") ;
-     $("#player2 h3").text("0");
-     $("#p2").hide();
-     $("#p22").hide();
-     $("#p1").show();
-     $("#p12").show();
-
-  }
-  
+     $("#player2 h3").text("0");$("#p2").hide();$("#p22").hide();$("#p1").show(); $("#p12").show();
+      // diceImage();
+  } 
 })
 
 $("#p22").click(function(){
   player2.hold();
+  if(player2.totalScore>=100){
+    $("#alert-winner2").text(" YOU'RE THE WINNER!!!");
+    $("#player2 h3").text("0");
+    $("#player2 h4").text("TOTAL SCORE:"+" "+ player2.totalScore);
+    $("#p2").hide();$("#p22").hide();$("#p1").hide();$("#p12").hide();
+  }else{
   player2.currentRound = [];
   $("#player2 h3").text("0");
-  $("#player2 h4").text("your score for this round is"+" "+ player2.totalScore);
-  $("#p2").hide();
-  $("#p22").hide();
-  $("#p1").show();
-  $("#p12").show();
+  $("#player2 h4").text("TOTAL SCORE:"+" "+ player2.totalScore);
+  $("#p2").hide();$("#p22").hide();$("#p1").show();$("#p12").show();}
 })
 
 $("#restart").click(function(){
   player1.totalScore = 0;
   player2.totalScore = 0;
   
-  $("#player1 h3").text("0");
-  $("#player1 h4").text("0");
-  $("#player2 h3").text("0");
-  $("#player2 h4").text("0");
-  $("#p2").show();
-  $("#p22").show();
-  $("#p1").show();
-  $("#p12").show();
-
- 
+  $("#player1 h3").text("0");$("#player1 h4").text("0");$("#player2 h3").text("0");$("#player2 h4").text("0");
+  $("#p2").show();$("#p22").show();$("#p1").show();$("#p12").show();
 }) 
 })
